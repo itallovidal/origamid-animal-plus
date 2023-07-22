1bf17dcc864b50eb7208299daf78ce75135817f1
itallovidal <itallovidalll@gmail.com>
2023-07-22 02:28:18 -0300

    scroll infinito

--- src/pages/home/components/cardPostagem.jsx
+++ src/pages/home/components/cardPostagem.jsx
@@ -7,7 +7,7 @@ function CardPostagem({dados}) {
 
     return (
         <picture key={dados.id} onClick={()=> storage.setModal(dados)} className={'card_post show'}>
-            <img src={dados.src} alt={dados.title}/>
+            <img src={dados.src} alt={dados.title} />
             <p> <span className={'watched'}>{dados.acessos}</span> <i className="fa-solid fa-eye"></i> </p>
         </picture>
     );
